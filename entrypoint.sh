3f43680aa47d512b3436df5fe423d3bff2a8ec45
AleixMT <aleixaretra@gmail.com>
2025-03-25 16:38:51 +0100

    ADDED: docker compose example usage, changed entrypoint for more flexibility around formats and delivery of projects

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,53 @@
 #!/bin/bash
 
+copy_binaries()
+{
+  find "$1" -type f -name "*.nds" -exec cp {} /output/ \;
+  find "$1" -type f -name "*.elf" -exec cp {} /output/ \;
+  find "$1" -type f -name "*.dldi" -exec cp {} /output/ \;
+}
+
 process_dir()
 {
+  DIR_MULTIPLE=0
   # Iterate over all directories inside $1
   for dir in $1/*/; do
       # Check if it's a directory
       if [ -d "$dir" ]; then
-          DIR_PRESENT=1
+          DIR_MULTIPLE=1
           echo "Processing directory: $dir"
           {
             cd "$dir"
             make clean
             make
+            COMPILED=1
           }
-          # Find and copy all .nds files from this directory to /output
-          find "$dir" -type f -name "*.nds" -exec cp {} /output/ \;
+          # Find and copy all binary files from this directory to /output
+          copy_binaries "${dir}"
       fi
   done
+
+  if [ "${DIR_MULTIPLE}" -eq 0 ]: then
+    {
+      cd "$1"
+      make clean
+      make
+    }
+    copy_binaries "$1"
+  fi
 }
 
-DIR_PRESENT=0
+
+set -e
+
+COMPILED=0
 
 process_dir /input
 process_dir /workspace
 
-if [ $DIR_PRESENT == 1 ]; then
-    echo "All .nds files copied to /output."
+if [ ${COMPILED} == 1 ]; then
+    echo "All binary files (.nds, .elf and .dldi) copied to /output."
 else
-    echo "No directories present in /input to compile. Entering interactive mode"
+    echo "No directories or content present in /input or /workspace to compile. Entering interactive mode"
     /bin/bash
 fi
