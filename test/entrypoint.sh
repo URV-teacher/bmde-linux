5713d1ff5369342bd7acd1aeca53145dc48c88d7
AleixMT <aleixaretra@gmail.com>
2025-03-25 16:06:22 +0100

    ADDED: test

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
-process_dir()
+test_checksums()
 {
-  # Iterate over all directories inside $1
-  for dir in $1/*/; do
-      # Check if it's a directory
-      if [ -d "$dir" ]; then
-          DIR_PRESENT=1
-          echo "Processing directory: $dir"
-          {
-            cd "$dir"
-            make clean
-            make
-          }
-          # Find and copy all .nds files from this directory to /output
-          find "$dir" -type f -name "*.nds" -exec cp {} /output/ \;
-      fi
-  done
+    win="$(sha1sum "$1" | cut -f1 -d " " )"
+    nix="$(sha1sum "$2" | cut -f1 -d " " )"
+    if [ "${win}" == "${nix}" ]; then
+      TESTS_PASSED=$(( TESTS_PASSED + 1))
+    else
+      msg="${NEWLINE}Test failed for $1 ${win} different than $2 ${nix}"
+      SUMMARY+="${msg}"
+      OUTPUT_MSG+="${msg}${NEWLINE}${NEWLINE}Failed with message:${NEWLINE}${comp_msg}"
+    fi
+    TOTAL_TESTS=$(( TOTAL_TESTS + 1))
 }
 
-DIR_PRESENT=0
+TOTAL_TESTS=0
+TESTS_PASSED=0
+OUTPUT_MSG=""
+NEWLINE=$'\n'
+SUMMARY=""
 
-process_dir /input
-process_dir /workspace
+cd test/roms
+for i in *; do
+  if [ "$i" == "Makefile" ]; then
+    continue
+  fi
+  comp_msg=$(cd "$i" && make clean && make 2>&1)
+  test_checksums "$i/win_build/$i.nds" "$i/$i.nds"
+done
 
-if [ $DIR_PRESENT == 1 ]; then
-    echo "All .nds files copied to /output."
+cd ../libs
+comp_msg=$(cd "arm9lib" && make clean && make 2>&1)
+test_checksums "arm9lib/win_build/libarm9lib.a" "arm9lib/lib/libarm9lib.a"
+comp_msg=$(cd "dldi" && make clean && make 2>&1)
+test_checksums "dldi/win_build/dldi.dldi" "dldi/dldi.dldi"
+
+if [ "${TOTAL_TESTS}" == "${TESTS_PASSED}" ]; then
+  echo "All tests passed."
 else
-    echo "No directories present in /input to compile. Entering interactive mode"
-    /bin/bash
+  echo -e "Make output for failed tests: ${OUTPUT_MSG}"
+  echo -e "${SUMMARY}"
+  echo "${TESTS_PASSED} out of ${TOTAL_TESTS} tests passed."
 fi
+
+
+
+
+
